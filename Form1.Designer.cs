edcb2c6c6ce5ceca69e71c88720fca4b5ff3842e
Tran Nam Long <tnamlong2132000@gmail.com>
2021-07-13 18:49:35 +0700

    bai long sua

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,25 @@ namespace PRN292_Project
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(78, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "long";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Login";
             this.ResumeLayout(false);
@@ -43,6 +55,8 @@ namespace PRN292_Project
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
